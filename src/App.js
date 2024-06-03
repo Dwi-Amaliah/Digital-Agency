98ccb71b6df6c102d55dd9b5f2fb9884dee2a078
AmaliahDwi <dwiamaliah30@gmail.com>
2024-06-03 17:08:48 +0700

    add form blog & team

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Header from "./components/Header/Header";
 import Hero from "./components/UI/Hero";
 import Counter from "./components/UI/Counter";
 import Service from "./components/UI/Service";
+import About from "./components/UI/About";
+import Team from "./components/UI/Team";
+import Blog from "./components/UI/Blog";
+import Testimonial from "./components/UI/Testimonial";
 
 function App() {
   return (
@@ -11,6 +15,10 @@ function App() {
       <Hero />
       <Counter />
       <Service />
+      <About />
+      <Team />
+      <Blog />
+      <Testimonial />
     </>
   );
 }
---
+++ src/components/UI/Testimonial.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "../../styles/testimonial.css";
+import Slider from "react-slick";
+import ava01 from "../../images/ava-1.jpg";
+
+const Testimonial = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="slider__content-top">
+          <h6 className="subtitle">Testimonials</h6>
+          <h2>
+            Trusted by more than{" "}
+            <span className="highlight">5,000 customers</span>
+          </h2>
+        </div>
+
+        <div className="slider_wrapper">
+          <Slider>
+            <div className="slider__item">
+              <p className="description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </p>
+
+              <div className="customer__details">
+                <div className="customer__img">
+                  <img src={ava01} alt="" />
+                </div>
+
+                <div>
+                  <h5 className="customer__name">Jhon Doe</h5>
+                </div>
+              </div>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonial;
